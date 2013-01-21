2e9524c1dba0012c538ec2602b4c0f12b2aeb5d5
Jules TESTARD <jtesta@lab2-15.CS.McGill.CA>
2013-01-20 19:29:19 -0500

    Michael Jackson

--- BubbleEater/src/InGameShapeManager.java
+++ BubbleEater/src/InGameShapeManager.java
@@ -1,4 +1,9 @@
 // Will replace and manage ingameshapes everywhere.
 public class InGameShapeManager {
 
+	private ArrayList<InGameShape> shapes;
+
+	public String michaeljackson() {
+		return "Billie Jean's, got my luv";
+	}
 }
