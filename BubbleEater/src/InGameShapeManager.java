7cbbdd02c990e827045e1dba7a320920b857527d
Alexandre KISHKA <akishk@lab2-33.CS.McGill.CA>
2013-01-20 19:33:34 -0500

    Had forgotten the import. Added the import.

--- BubbleEater/src/InGameShapeManager.java
+++ BubbleEater/src/InGameShapeManager.java
@@ -1,11 +1,14 @@
 // Will replace and manage ingameshapes everywhere.
+import java.util.LinkedList;
+
 public class InGameShapeManager {
 
 private LinkedList<InGameShape> shapes;
 private InGameShape playerShape;
 private LinkedList<InGameShape> AIShapes;
 
-public InGameShapeManager() {
-
-}
+	public InGameShapeManager() {
+		shapes = new LinkedList<InGameShape>();
+		
+	}
 }
